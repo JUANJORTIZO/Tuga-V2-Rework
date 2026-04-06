e87976ca917523b7628a94b0f655dda47ed0b68c
JUANJORTIZO <143625807+JUANJORTIZO@users.noreply.github.com>
2026-04-06 09:43:52 -0500

    actualizacion del proyecto

--- src/pages/UsuarioDetalle.jsx
+++ src/pages/UsuarioDetalle.jsx
@@ -34,41 +34,33 @@ export default function UsuarioDetalle() {
       <BackgroundLayout>
         <Panel>
           <h2 className="text-2xl md:text-3xl font-serif-title text-center mb-8">
-            Datos del usuario
+            Datos usuario
           </h2>
 
           <div className="grid grid-cols-1 md:grid-cols-3 gap-x-6 gap-y-5">
-            <FormInput label="Código:" value={user.code || ''} onChange={() => {}} readOnly />
             <FormInput label="Tipo de documento:" value={user.tipoDocumento || ''} onChange={() => {}} readOnly />
-            <FormInput label="Número de documento:" value={user.numeroDocumento || ''} onChange={() => {}} readOnly />
+            <FormInput label="Numero de documento:" value={user.numeroDocumento || ''} onChange={() => {}} readOnly />
+            <FormInput label="Lugar expedicion:" value={user.lugarExpedicion || ''} onChange={() => {}} readOnly />
 
-            <FormInput label="Lugar expedición:" value={user.lugarExpedicion || ''} onChange={() => {}} readOnly />
             <FormInput label="Nombres:" value={user.nombres || ''} onChange={() => {}} readOnly />
             <FormInput label="Apellidos:" value={user.apellidos || ''} onChange={() => {}} readOnly />
+            <FormInput label="Genero:" value={user.genero || ''} onChange={() => {}} readOnly />
 
-            <FormInput label="Género:" value={user.genero || ''} onChange={() => {}} readOnly />
-            <FormInput label="Correo electrónico:" value={user.correo || ''} onChange={() => {}} readOnly className="md:col-span-2" />
-            <FormInput label="Teléfono:" value={user.telefono || ''} onChange={() => {}} readOnly />
+            <FormInput
+              label="Correo Electronico:"
+              value={user.correo || ''}
+              onChange={() => {}}
+              readOnly
+              className="md:col-span-2"
+            />
 
-            <FormInput label="Dirección:" value={user.direccion || ''} onChange={() => {}} readOnly />
+            <FormInput label="Telefono:" value={user.telefono || ''} onChange={() => {}} readOnly />
+            <FormInput label="Direccion:" value={user.direccion || ''} onChange={() => {}} readOnly />
             <FormInput label="Departamento:" value={user.departamento || ''} onChange={() => {}} readOnly />
             <FormInput label="Municipio:" value={user.municipio || ''} onChange={() => {}} readOnly />
-
             <FormInput label="Estrato:" value={user.estrato || ''} onChange={() => {}} readOnly />
-            <FormInput label="Población vulnerable:" value={user.poblacionVulnerable || ''} onChange={() => {}} readOnly />
+            <FormInput label="Poblacion vulnerable:" value={user.poblacionVulnerable || ''} onChange={() => {}} readOnly />
             <FormInput label="Estado civil:" value={user.estadoCivil || ''} onChange={() => {}} readOnly />
-
-            <FormInput label="Empresa donde trabaja:" value={user.empresaDondeTrabaja || ''} onChange={() => {}} readOnly />
-            <FormInput label="Dirección empresa:" value={user.direccionEmpresa || ''} onChange={() => {}} readOnly />
-            <FormInput label="Teléfono empresa:" value={user.telefonoEmpresa || ''} onChange={() => {}} readOnly />
-
-            <FormInput label="Ciudad empresa:" value={user.ciudadEmpresa || ''} onChange={() => {}} readOnly />
-            <FormInput label="Sueldo:" value={String(user.sueldo || '')} onChange={() => {}} readOnly />
-            <FormInput label="Otros ingresos:" value={String(user.otrosIngresos || '')} onChange={() => {}} readOnly />
-
-            <FormInput label="Por concepto de:" value={user.porConceptoDe || ''} onChange={() => {}} readOnly />
-            <FormInput label="Tiene vivienda propia:" value={user.tieneViviendaPropia || ''} onChange={() => {}} readOnly />
-            <FormInput label="Avalúo catastral:" value={user.avaluoCatastral || ''} onChange={() => {}} readOnly />
           </div>
 
           <div className="mt-8">
